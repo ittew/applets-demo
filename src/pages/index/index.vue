30921922e9440e67c310e7ae652846989a60eba2
yangnan <yangnan@leadeon.cn>
2020-04-23 19:41:34 +0800

    fix:首页和列表页开发

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,126 +1,411 @@
 <template>
-  <div @click="clickHandle">
-
-    <div class="userinfo" @click="bindViewTap">
-      <img class="userinfo-avatar" v-if="userInfo.avatarUrl" :src="userInfo.avatarUrl" background-size="cover" />
-      <img class="userinfo-avatar" src="/static/images/user.png" background-size="cover" />
-
-      <div class="userinfo-nickname">
-        <card :text="userInfo.nickName"></card>
+  <div class="index">
+    <!-- 搜索框 -->
+    <div class="search">
+        <span class="icon-search"></span>
+        <input placeholder="搜索你想听的节目">
+    </div>
+    <!-- 轮播图 -->
+    <div class="swiper-container">
+      <swiper class="swiper" autoplay="true" circular="true"
+        interval="3000" easing-function="easeOutCubic"
+        @change="swiperChange">
+        <!-- 轮播图图片 -->
+        <swiper-item class="swiperItem" v-for="(v,i) in imgList" :key="i">
+          <!-- 加上widthFix可以使图片自适应 -->
+          <img :src="v" alt="">
+        </swiper-item>
+      </swiper>
+      <!-- 为了和喜马拉雅一致，自己需定制轮播图dots -->
+      <div class="dots">
+        <div v-for="(v,i) in imgList" :key="i">
+          <div class="dotsItem" :class="i == swiperCurrent ? ' dotActive' : ''"></div>
+        </div>
       </div>
     </div>
-
-    <div class="usermotto">
-      <div class="user-motto">
-        <card :text="motto"></card>
+    <!-- 猜你喜欢 -->
+    <div class="like">
+      <div class="barTitle">
+        <div class="Title-left">猜你喜欢</div>
+        <div class="Title-right" @tap="gotoList">查看全部 <text class="icon-right"></text> </div>
+      </div>
+      <div class="likeItemBox">
+        <div class="likeItem" @tap="gotoDetails"  v-for="(v,i) in guess" :key="i">
+          <div class="likeimg">
+            <img class="likeItemIcon" :src="v.coverMiddle">
+            <div class="likecover">
+              <!-- text用来显示icon  暂无 -->
+              <text class=""></text> 4.6亿
+            </div>
+          </div>
+          <div class="likeText">{{v.intro}}</div>
+        </div>
       </div>
     </div>
-
-    <form class="form-container">
-      <input type="text" class="form-control" :value="motto" placeholder="v-model" />
-      <input type="text" class="form-control" v-model="motto" placeholder="v-model" />
-      <input type="text" class="form-control" v-model.lazy="motto" placeholder="v-model.lazy" />
-    </form>
-
-    <a href="/pages/counter/main" class="counter">去往Vuex示例页面</a>
-
-    <div class="all">
-        <div class="left">
+    <!-- 有声小说 -->
+    <div class="contentList">
+      <div class="barTitle">
+        <div class="Title-left">有声小说</div>
+        <div class="Title-right" @tap="gotoList">查看全部<text class="icon-right"></text></div>
+      </div>
+      <div v-for="(item,index) in novel" :key="index">
+        <div class="content" @tap="gotoDetails">
+          <div class="contentImg">
+            <image :src="item.albumCoverUrl290" mode="widthFix"></image>
+          </div>
+          <div class="content-right">
+            <div class="content-title">
+              <div class="titleText">{{item.title}}</div>
+              <div class="introduction">{{item.trackTitle}}</div>
+            </div>
+            <div class="count">
+              <div class="playcount">
+                <!-- <text class="icon-"></text> -->
+                126亿
+              </div>
+              <div class="jicount">{{item.tracks}}集</div>
+            </div>
+          </div>
         </div>
-        <div class="right">
+      </div>
+    </div>
+    <!-- 旅游 -->
+    <div class="contentList">
+      <div class="barTitle">
+        <div class="Title-left">旅游</div>
+        <div class="Title-right" @tap="gotoList">查看全部<text class="icon-right"></text></div>
+      </div>
+      <div v-for="(item,index) in novel" :key="index">
+        <div class="content" @tap="gotoDetails">
+          <div class="contentImg">
+            <image :src="item.albumCoverUrl290" mode="widthFix"></image>
+          </div>
+          <div class="content-right">
+            <div class="content-title">
+              <div class="titleText">{{item.title}}</div>
+              <div class="introduction">{{item.trackTitle}}</div>
+            </div>
+            <div class="count">
+              <div class="playcount">
+                <!-- <text class="icon-"></text> -->
+                126亿
+              </div>
+              <div class="jicount">{{item.tracks}}集</div>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import card from '@/components/card'
 
 export default {
   data () {
     return {
-      motto: 'Hello miniprograme',
-      userInfo: {
-        nickName: 'mpvue',
-        avatarUrl: 'http://mpvue.com/assets/logo.png'
-      }
+      imgList: [
+        '/static/images/index/ad1.jpg',
+        '/static/images/index/ad2.jpg',
+        '/static/images/index/ad3.jpg',
+        '/static/images/index/ad4.jpg',
+        '/static/images/index/ad5.jpg',
+        '/static/images/index/ad6.jpg',
+        '/static/images/index/ad7.jpg'
+      ],
+      guess: [
+        {
+          'intro': '聪明宝宝从胎教音乐开始',
+          'coverMiddle': 'http://imagev2.xmcdn.com/group32/M08/28/BB/wKgJS1muRorAI1jPAAKG8d9Exac241.png!op_type=5&upload_type=album&device_type=ios&name=medium&magick=png'
+        }, {
+          'intro': '聪明宝宝从胎教音乐开始',
+          'coverMiddle': 'http://imagev2.xmcdn.com/group32/M08/28/BB/wKgJS1muRorAI1jPAAKG8d9Exac241.png!op_type=5&upload_type=album&device_type=ios&name=medium&magick=png'
+        }, {
+          'intro': '聪明宝宝从胎教音乐开始',
+          'coverMiddle': 'http://imagev2.xmcdn.com/group32/M08/28/BB/wKgJS1muRorAI1jPAAKG8d9Exac241.png!op_type=5&upload_type=album&device_type=ios&name=medium&magick=png'
+        }
+      ],
+      novel: [
+        {
+          'albumCoverUrl290': 'http://imagev2.xmcdn.com/group66/M02/5F/87/wKgMdV1k-SryG2bxAANsUR5aOuY490.jpg!op_type=5&upload_type=album&device_type=ios&name=medium&magick=png',
+          'title': '《长宁帝军》| 江湖范儿热血爽文',
+          'trackTitle': '今日，谁与我共同浴血，谁就是我的兄弟',
+          'tracks': 100
+        },
+        {
+          'albumCoverUrl290': 'http://imagev2.xmcdn.com/group66/M02/5F/87/wKgMdV1k-SryG2bxAANsUR5aOuY490.jpg!op_type=5&upload_type=album&device_type=ios&name=medium&magick=png',
+          'title': '《长宁帝军》| 江湖范儿热血爽文',
+          'trackTitle': '今日，谁与我共同浴血，谁就是我的兄弟',
+          'tracks': 100
+        },
+        {
+          'albumCoverUrl290': 'http://imagev2.xmcdn.com/group66/M02/5F/87/wKgMdV1k-SryG2bxAANsUR5aOuY490.jpg!op_type=5&upload_type=album&device_type=ios&name=medium&magick=png',
+          'title': '《长宁帝军》| 江湖范儿热血爽文',
+          'trackTitle': '今日，谁与我共同浴血，谁就是我的兄弟',
+          'tracks': 100
+        }
+      ],
+      swiperCurrent: 0
     }
   },
-
-  components: {
-    card
-  },
-
   methods: {
-    bindViewTap () {
-      const url = '../logs/main'
-      if (mpvuePlatform === 'wx') {
-        mpvue.switchTab({ url })
-      } else {
-        mpvue.navigateTo({ url })
-      }
+    // 轮播图改变事件
+    swiperChange (e) {
+      this.swiperCurrent = e.mp.detail.current
     },
-    clickHandle (ev) {
-      console.log('clickHandle:', ev)
-      // throw {message: 'custom test'}
+    // 跳转商品详情页面
+    gotoDetails () {
+      // var url = e.currentTarget.dataset.coverimg
+      // var title = e.currentTarget.dataset.title
+      wx.navigateTo({
+        url: '/pages/detail/main'
+      })
+    },
+    // 跳转商品详情页面
+    gotoList () {
+      console.log(5555)
+      // var url = e.currentTarget.dataset.coverimg
+      // var title = e.currentTarget.dataset.title
+      wx.navigateTo({
+        url: '/pages/list/main'
+      })
     }
-  },
-
-  created () {
-    // let app = getApp()
   }
 }
 </script>
+<style lang="scss">
+  page {
+    // background-color: #0f0;
+    height: 100%;
+  }
+  /* 搜索框 */
+  .search{
+    width: 90%;
+    display: flex;
+    align-items: center;
+    margin: 0 auto;
+    // margin-left: 5%;
+    // margin-top: 20px;
+    background-color: rgb(240, 240, 240);
+    padding: 10rpx 20rpx;
+    border-radius: 15rpx;
+  }
 
-<style scoped lang='scss'>
-.userinfo {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
+  .search input{
+    width: 90%;
+    height: 100%;
+    font-size: 29rpx;
+    margin-left: 20rpx;
+  }
+  // 轮播图
+  .swiper-container{
+    width: 90%;
+    height: 280rpx;
+    margin: 0rpx auto;
+    margin-top: 30px;
+    position: relative;
+    border-radius: 15rpx;
+    overflow: hidden;
+  }
+  .swiper{
+    width: 100%;
+    height: 100%;
+    border-radius: 15rpx;
+  }
+  .swiperItem{
+    width: 100%;
+    height: 100%;
+    border-radius: 15rpx;
+  }
+  .swiperItem>img{
+    width: 100%;
+    height: 100%;
+    border-radius: 15rpx;
+  }
+  .dots{
+    width: auto;
+    height: 10rpx;
+    position: absolute;
+    bottom: 20rpx;
+    border-radius: 50rpx;
+    left: 240rpx;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 3rpx 10rpx;
+  }
+  .dotsItem{
+    width: 8rpx;
+    height: 8rpx;
+    margin: 0rpx 8rpx;
+    border-radius: 50%;
+    background-color: #cdcdcd;
+    transition: all .3s;
+  }
+  .dotActive{
+    width: 10rpx;
+    height: 10rpx;
+    background-color: #fff;
+  }
+  // 猜你喜欢
+  .barTitle {
+    width: 100%;
+    height: 56rpx;
+    padding: 20rpx 0rpx;
+    text-align: center;
+  }
+  .Title-left {
+    float: left;
+    font-size: 37rpx;
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    font-weight: 800;
+    color: #333;
+  }
 
-.userinfo-avatar {
-  width: 128rpx;
-  height: 128rpx;
-  margin: 20rpx;
-  border-radius: 50%;
-}
-
-.userinfo-nickname {
-  color: #aaa;
-}
-
-.usermotto {
-  margin-top: 150px;
-}
-
-.form-control {
-  display: block;
-  padding: 0 12px;
-  margin-bottom: 5px;
-  border: 1px solid #ccc;
-}
-.all{
-  width:7.5rem;
-  height:1rem;
-  background-color:blue;
-}
-.all:after{
-  display:block;
-  content:'';
-  clear:both;
-}
-.left{
-  float:left;
-  width:3rem;
-  height:1rem;
-  background-color:red;
-}
-
-.right{
-  float:left;
-  width:4.5rem;
-  height:1rem;
-  background-color:green;
-}
+  .Title-right {
+    float: right;
+    font-size: 26rpx;
+    color: #aaa;
+  }
+  .like{
+    width: 90%;
+    margin: 0rpx auto;
+    margin-top: 40rpx;
+  }
+  .likeItemBox{
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    overflow: hidden;
+  }
+  .likeItem{
+    width: 210rpx;
+    height: 310rpx;
+    border-radius: 15rpx;
+  }
+  .likeimg{
+    width: 210rpx;
+    height: 210rpx;
+    border-radius: 15rpx;
+    position: relative;
+  }
+  .likeItemIcon{
+    width: 100%;
+    height: 100%;
+    border-radius: 15rpx;
+  }
+  .likeText{
+    font-size: 28rpx;
+    color: #333;
+    margin-top: 10rpx;
+    display: -webkit-box;
+    -webkit-line-clamp: 2;/*行数n*/
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+  .likecover{
+    width: 190rpx;
+    height: 40rpx;
+    /* 透明背景层 */
+    background-image: linear-gradient(rgba(0, 0, 0, 0.01), rgba(0, 0, 0, 1));
+    /* opacity: 0.3; */
+    position: absolute;
+    bottom: 0rpx;
+    color: #fff;
+    font-size: 25rpx;
+    padding: 5rpx 10rpx;
+    border-bottom-left-radius: 15rpx;
+    border-bottom-right-radius: 15rpx;
+  }
+  .dataNotip{
+    width: 100%;
+    height: 300rpx;
+    padding-top: 150rpx;
+  }
+  .tip{
+    width: auto;
+    height: 100rpx;
+    text-align: center;
+    margin: 0rpx auto;
+    color: red;
+    font-weight: 800;
+  }
+  /* 有声小说-主要内容列表样式 */
+  .contentList{
+    width: 90%;
+    height: auto;
+    margin: 0rpx auto;
+  }
+  .content{
+    width: 100%;
+    height: auto;
+    padding: 20rpx 0rpx;
+    display: flex;
+    justify-content: space-between;
+    border-bottom: 1rpx solid #f1f1f1;
+  }
+  .contentImg{
+    width: 232rpx;
+    height: 232rpx;
+    border-radius: 15rpx;
+  }
+  .contentImg>image{
+    width: 100%;
+    height: 100%;
+    border-radius: 15rpx;
+  }
+  .content-right{
+    width: 530rpx;
+    height: 100%;
+    margin-left: 20rpx;
+    display: flex;
+    flex-direction: column;
+  }
+  .content-title{
+    width: 100%;
+    height: 138rpx;
+    overflow: hidden;
+    display: flex;
+    flex-direction: column;
+  }
+  .titleText{
+    width: 100%;
+    font-size: 35rpx;
+    color: #333;
+    text-align: left;
+    display: -webkit-box;
+    -webkit-line-clamp: 1;/*行数n*/
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+  .introduction{
+    width: 100%;
+    font-size: 28rpx;
+    color: #cdcdcd;
+    text-align: left;
+    padding: 15rpx 0rpx;
+    display: -webkit-box;
+    -webkit-line-clamp: 2;/*行数n*/
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+  .count{
+    width: 100%;
+    height: 50rpx;
+    display: flex;
+    justify-content: start;
+    align-items: center;
+  }
+  .playcount{
+  font-size: 25rpx;
+  color: #cdcdcd;
+  margin-right: 30rpx;
+  }
+  .jicount{
+    font-size: 25rpx;
+    color: #cdcdcd;
+  }
 </style>
---
+++ src/pages/list/list.vue
@@ -0,0 +1,318 @@
+<template>
+  <div class="list">
+    <div class="top-nav">
+      <div class="navigate-item" v-for="(item,index) in navTitle" :key="index" @tap="handleClick(index)">
+        <div class="names" :class="currentTab === index ? 'active' : ''">{{item.title}}</div>
+        <div class="currtline" :class="currentTab === index ? 'active' : ''"></div>
+      </div>
+    </div>
+    <!-- 对应的内容区 -->
+    <!-- <scroll-view scroll-y> -->
+      <div class="content-item"  v-for="(item,index) in currentList" :key="index">
+        <div class="indexNumber">
+          <div>{{index > 10 ? index : '0'+(index+1)}}</div>
+        </div>
+        <image :src="item.albumCoverUrl290"></image>
+        <div class="content-left">
+          <div class="content-title">{{item.title}}</div>
+          <div class="content-text">{{item.trackTitle}}</div>
+          <div class="count">
+            <div style="margin-right:25rpx;">
+              <!-- 本例中33.2亿的单位是写死的，此处应该是后端返回数据吧 -->
+              <text class="icon-video"></text>
+              <text class="">33.2亿</text>
+            </div>
+            <div>
+              <text class="icon"></text>
+              <text class="">44集</text>
+            </div>
+          </div>
+        </div>
+      </div>
+    <!-- </scroll-view> -->
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      currentTab: 0,
+      navTitle: [
+        {title: '儿童教育'},
+        {title: '人文历史'},
+        {title: '最潮外语'}
+      ],
+      currentList: [],
+      listData: [
+        {
+          'hotRecommends': {
+            'list': [
+              {
+                'albumCoverUrl290': 'http://imagev2.xmcdn.com/group66/M02/5F/87/wKgMdV1k-SryG2bxAANsUR5aOuY490.jpg!op_type=5&upload_type=album&device_type=ios&name=medium&magick=png',
+                'title': '《长宁帝军》| 江湖范儿热血爽文',
+                'trackTitle': '今日，谁与我共同浴血，谁就是我的兄弟'
+              },
+              {
+                'albumCoverUrl290': 'http://imagev2.xmcdn.com/group59/M0A/AE/A4/wKgLel0_6jSB96guAAQiVnZ8T6o580.jpg!op_type=5&upload_type=album&device_type=ios&name=medium&magick=png',
+                'title': '绝世高手【头陀渊】免费有声小说',
+                'trackTitle': '男女双播，都市玄幻，修真神作'
+              },
+              {
+                'albumCoverUrl290': 'http://imagev2.xmcdn.com/group59/M0A/AE/A4/wKgLel0_6jSB96guAAQiVnZ8T6o580.jpg!op_type=5&upload_type=album&device_type=ios&name=medium&magick=png',
+                'title': '绝世高手【头陀渊】免费有声小说',
+                'trackTitle': '男女双播，都市玄幻，修真神作'
+              },
+              {
+                'albumCoverUrl290': 'http://imagev2.xmcdn.com/group59/M0A/AE/A4/wKgLel0_6jSB96guAAQiVnZ8T6o580.jpg!op_type=5&upload_type=album&device_type=ios&name=medium&magick=png',
+                'title': '绝世高手【头陀渊】免费有声小说',
+                'trackTitle': '男女双播，都市玄幻，修真神作'
+              },
+              {
+                'albumCoverUrl290': 'http://imagev2.xmcdn.com/group59/M0A/AE/A4/wKgLel0_6jSB96guAAQiVnZ8T6o580.jpg!op_type=5&upload_type=album&device_type=ios&name=medium&magick=png',
+                'title': '绝世高手【头陀渊】免费有声小说',
+                'trackTitle': '男女双播，都市玄幻，修真神作'
+              },
+              {
+                'albumCoverUrl290': 'http://imagev2.xmcdn.com/group59/M0A/AE/A4/wKgLel0_6jSB96guAAQiVnZ8T6o580.jpg!op_type=5&upload_type=album&device_type=ios&name=medium&magick=png',
+                'title': '绝世高手【头陀渊】免费有声小说',
+                'trackTitle': '男女双播，都市玄幻，修真神作'
+              },
+              {
+                'albumCoverUrl290': 'http://imagev2.xmcdn.com/group59/M0A/AE/A4/wKgLel0_6jSB96guAAQiVnZ8T6o580.jpg!op_type=5&upload_type=album&device_type=ios&name=medium&magick=png',
+                'title': '绝世高手【头陀渊】免费有声小说',
+                'trackTitle': '男女双播，都市玄幻，修真神作'
+              },
+              {
+                'albumCoverUrl290': 'http://imagev2.xmcdn.com/group59/M0A/AE/A4/wKgLel0_6jSB96guAAQiVnZ8T6o580.jpg!op_type=5&upload_type=album&device_type=ios&name=medium&magick=png',
+                'title': '绝世高手【头陀渊】免费有声小说',
+                'trackTitle': '男女双播，都市玄幻，修真神作'
+              },
+              {
+                'albumCoverUrl290': 'http://imagev2.xmcdn.com/group59/M0A/AE/A4/wKgLel0_6jSB96guAAQiVnZ8T6o580.jpg!op_type=5&upload_type=album&device_type=ios&name=medium&magick=png',
+                'title': '绝世高手【头陀渊】免费有声小说',
+                'trackTitle': '男女双播，都市玄幻，修真神作'
+              }
+            ]
+          }
+        },
+        {
+          'hotRecommends': {
+            'list': [
+              {
+                'albumCoverUrl290': 'http://imagev2.xmcdn.com/group66/M02/5F/87/wKgMdV1k-SryG2bxAANsUR5aOuY490.jpg!op_type=5&upload_type=album&device_type=ios&name=medium&magick=png',
+                'title': '《长宁帝军》| 江湖范儿热血爽文',
+                'trackTitle': '今日，谁与我共同浴血，谁就是我的兄弟'
+              },
+              {
+                'albumCoverUrl290': 'http://imagev2.xmcdn.com/group59/M0A/AE/A4/wKgLel0_6jSB96guAAQiVnZ8T6o580.jpg!op_type=5&upload_type=album&device_type=ios&name=medium&magick=png',
+                'title': '22222222222222222222',
+                'trackTitle': '男女双播，都市玄幻，修真神作'
+              },
+              {
+                'albumCoverUrl290': 'http://imagev2.xmcdn.com/group59/M0A/AE/A4/wKgLel0_6jSB96guAAQiVnZ8T6o580.jpg!op_type=5&upload_type=album&device_type=ios&name=medium&magick=png',
+                'title': '22222222222222222222',
+                'trackTitle': '男女双播，都市玄幻，修真神作'
+              },
+              {
+                'albumCoverUrl290': 'http://imagev2.xmcdn.com/group59/M0A/AE/A4/wKgLel0_6jSB96guAAQiVnZ8T6o580.jpg!op_type=5&upload_type=album&device_type=ios&name=medium&magick=png',
+                'title': '22222222222222222222',
+                'trackTitle': '男女双播，都市玄幻，修真神作'
+              },
+              {
+                'albumCoverUrl290': 'http://imagev2.xmcdn.com/group59/M0A/AE/A4/wKgLel0_6jSB96guAAQiVnZ8T6o580.jpg!op_type=5&upload_type=album&device_type=ios&name=medium&magick=png',
+                'title': '2222222222222222',
+                'trackTitle': '男女双播，都市玄幻，修真神作'
+              },
+              {
+                'albumCoverUrl290': 'http://imagev2.xmcdn.com/group59/M0A/AE/A4/wKgLel0_6jSB96guAAQiVnZ8T6o580.jpg!op_type=5&upload_type=album&device_type=ios&name=medium&magick=png',
+                'title': '2222222222222222',
+                'trackTitle': '男女双播，都市玄幻，修真神作'
+              },
+              {
+                'albumCoverUrl290': 'http://imagev2.xmcdn.com/group59/M0A/AE/A4/wKgLel0_6jSB96guAAQiVnZ8T6o580.jpg!op_type=5&upload_type=album&device_type=ios&name=medium&magick=png',
+                'title': '22222222222222222222222222222222',
+                'trackTitle': '男女双播，都市玄幻，修真神作'
+              },
+              {
+                'albumCoverUrl290': 'http://imagev2.xmcdn.com/group59/M0A/AE/A4/wKgLel0_6jSB96guAAQiVnZ8T6o580.jpg!op_type=5&upload_type=album&device_type=ios&name=medium&magick=png',
+                'title': '22222222222222222222222222222222',
+                'trackTitle': '男女双播，都市玄幻，修真神作'
+              },
+              {
+                'albumCoverUrl290': 'http://imagev2.xmcdn.com/group59/M0A/AE/A4/wKgLel0_6jSB96guAAQiVnZ8T6o580.jpg!op_type=5&upload_type=album&device_type=ios&name=medium&magick=png',
+                'title': '222222222222',
+                'trackTitle': '男女双播，都市玄幻，修真神作'
+              }
+            ]
+          }
+        },
+        {
+          'hotRecommends': {
+            'list': [
+              {
+                'albumCoverUrl290': 'http://imagev2.xmcdn.com/group66/M02/5F/87/wKgMdV1k-SryG2bxAANsUR5aOuY490.jpg!op_type=5&upload_type=album&device_type=ios&name=medium&magick=png',
+                'title': '《长宁帝军》| 江湖范儿热血爽文',
+                'trackTitle': '今日，谁与我共同浴血，谁就是我的兄弟'
+              },
+              {
+                'albumCoverUrl290': 'http://imagev2.xmcdn.com/group59/M0A/AE/A4/wKgLel0_6jSB96guAAQiVnZ8T6o580.jpg!op_type=5&upload_type=album&device_type=ios&name=medium&magick=png',
+                'title': '33333333333333333',
+                'trackTitle': '男女双播，都市玄幻，修真神作'
+              },
+              {
+                'albumCoverUrl290': 'http://imagev2.xmcdn.com/group59/M0A/AE/A4/wKgLel0_6jSB96guAAQiVnZ8T6o580.jpg!op_type=5&upload_type=album&device_type=ios&name=medium&magick=png',
+                'title': '333333333333333333333333',
+                'trackTitle': '男女双播，都市玄幻，修真神作'
+              },
+              {
+                'albumCoverUrl290': 'http://imagev2.xmcdn.com/group59/M0A/AE/A4/wKgLel0_6jSB96guAAQiVnZ8T6o580.jpg!op_type=5&upload_type=album&device_type=ios&name=medium&magick=png',
+                'title': '333333333333333333333333',
+                'trackTitle': '男女双播，都市玄幻，修真神作'
+              },
+              {
+                'albumCoverUrl290': 'http://imagev2.xmcdn.com/group59/M0A/AE/A4/wKgLel0_6jSB96guAAQiVnZ8T6o580.jpg!op_type=5&upload_type=album&device_type=ios&name=medium&magick=png',
+                'title': '333333333333333333333333',
+                'trackTitle': '男女双播，都市玄幻，修真神作'
+              },
+              {
+                'albumCoverUrl290': 'http://imagev2.xmcdn.com/group59/M0A/AE/A4/wKgLel0_6jSB96guAAQiVnZ8T6o580.jpg!op_type=5&upload_type=album&device_type=ios&name=medium&magick=png',
+                'title': '333333333333333333333333',
+                'trackTitle': '男女双播，都市玄幻，修真神作'
+              },
+              {
+                'albumCoverUrl290': 'http://imagev2.xmcdn.com/group59/M0A/AE/A4/wKgLel0_6jSB96guAAQiVnZ8T6o580.jpg!op_type=5&upload_type=album&device_type=ios&name=medium&magick=png',
+                'title': '333333333333333333333333',
+                'trackTitle': '男女双播，都市玄幻，修真神作'
+              },
+              {
+                'albumCoverUrl290': 'http://imagev2.xmcdn.com/group59/M0A/AE/A4/wKgLel0_6jSB96guAAQiVnZ8T6o580.jpg!op_type=5&upload_type=album&device_type=ios&name=medium&magick=png',
+                'title': '333333333333333333333333',
+                'trackTitle': '男女双播，都市玄幻，修真神作'
+              },
+              {
+                'albumCoverUrl290': 'http://imagev2.xmcdn.com/group59/M0A/AE/A4/wKgLel0_6jSB96guAAQiVnZ8T6o580.jpg!op_type=5&upload_type=album&device_type=ios&name=medium&magick=png',
+                'title': '333333333333333333333333',
+                'trackTitle': '男女双播，都市玄幻，修真神作'
+              }
+            ]
+          }
+        }
+      ]
+    }
+  },
+  beforeMount () {
+    this.currentList = this.listData[0].hotRecommends.list
+  },
+  methods: {
+    // 跳转商品详情页面
+    handleClick (i) {
+      this.currentTab = i
+      this.currentList = this.listData[i].hotRecommends.list
+      console.log(this.currentList)
+    }
+  }
+}
+</script>
+<style lang="scss">
+  page {
+    // background-color: #0f0;
+    height: 100%;
+  }
+  .top-nav{
+    width: 100%;
+    height: 80rpx;
+    display: flex;
+  }
+  .navigate-item {
+    flex:1;
+    text-align: center;
+    height: 90rpx;
+    line-height: 90rpx;
+  }
+
+  .names {
+    font-size: 28rpx;
+    color: #3c3c3c;
+  }
+
+  .names.active {
+    color: #ff520f;
+    font-weight: bold;
+    font-size: 34rpx;
+  }
+
+  .currtline {
+    margin: -8rpx auto 0 auto;
+    width: 100rpx;
+    height: 1rpx;
+    border-radius: 4rpx;
+  }
+
+  .currtline.active {
+    background: #ff520f;
+    transition: all .3s;
+  }
+  .content-item{
+    width: 100%;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    padding: 20rpx 0rpx;
+  }
+
+  .indexNumber {
+    width: 15%;
+    height: auto;
+    padding: 20rpx 0rpx;
+    text-align: center;
+    font-size: 50rpx;
+    color: #ff520f;
+  }
+
+  .content-item image {
+    width: 140rpx;
+    height: 140rpx;
+    margin-top: 10rpx;
+    border-radius: 15rpx;
+  }
+
+  .content-left {
+    padding: 10rpx 0rpx;
+    margin-left: 30rpx;
+    width: 60%;
+    height: auto;
+    border-bottom: 1rpx solid #f1f1f1;
+  }
+
+  .content-title{
+    font-size: 35rpx;
+    height: 40rpx;
+    color: #000;
+    display: -webkit-box;
+    -webkit-line-clamp: 2;/*行数n*/
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+  .content-text{
+    height: 40rpx;
+    display: -webkit-box;
+    -webkit-line-clamp: 1;/*行数n*/
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    font-size: 27rpx;
+    text-align: left;
+    color: #ccc;
+    margin-top: 15rpx;
+  }
+  .count{
+    font-size: 27rpx;
+    color: #ccc;
+    display: flex;
+    justify-content: start;
+    margin-top: 15rpx;
+  }
+</style>
